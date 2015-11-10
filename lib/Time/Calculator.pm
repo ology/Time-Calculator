d1f6912938551a79ff252c69f601debfd74dae19
Gene Boggs <gene.boggs@gmail.com>
2015-11-10 17:13:08 -0600

    Repurpose the first input

--- lib/Time/Calculator.pm
+++ lib/Time/Calculator.pm
@@ -38,61 +38,58 @@ sub calculate {
     my %args = @_;
 
     my $op    = $args{op} // 'clear';
-    my $out   = $args{output};
     my $first = $args{first};
+    my $out   = $args{output};
 
     my $offset = $first =~ /\A\d+\z/ ? $first : 1;
 
-    my $format = DateTime::Format::Strptime->new(
-       pattern   => '%Y-%m-%dT%H:%M:%S',
-       time_zone => 'local',
-       on_error  => 'croak',
-    );
-    my $parsed = eval { $format->parse_datetime($out) };
-    my $dt = !$@ && params->{output}
-        ? $parsed
-        : DateTime->now(time_zone => 'local');
+    my ( $stamp, $first_dt, $out_dt );
+
+    if ( $first ) {
+        $stamp = UnixDate( $first, "%Y-%m-%eT%H:%M:%S" );
+        $first_dt = DateTime::Format::DateParse->parse_datetime($stamp);
+    }
+    if ( $out ) {
+        $stamp = UnixDate( $out, "%Y-%m-%eT%H:%M:%S" );
+        $out_dt = DateTime::Format::DateParse->parse_datetime($stamp);
+    }
 
     my $dispatch = {
         clear => sub { $out = ''; $first = ''; },
         now   => sub {
-            if ($out) {
+            if ( $out ) {
                 $first = DateTime->now(time_zone => 'local');
             }
             else {
                 $out = DateTime->now(time_zone => 'local');
             }
         },
-        dow             => sub { $out = $dt->day_name },
-        add_year        => sub { $out = $dt->add( years => $offset ) },
-        subtract_year   => sub { $out = $dt->subtract( years => $offset ) },
-        add_month       => sub { $out = $dt->add( months => $offset ) },
-        subtract_month  => sub { $out = $dt->subtract( months => $offset ) },
-        add_day         => sub { $out = $dt->add( days => $offset ) },
-        subtract_day    => sub { $out = $dt->subtract( days => $offset ) },
-        add_hour        => sub { $out = $dt->add( hours => $offset ) },
-        subtract_hour   => sub { $out = $dt->subtract( hours => $offset ) },
-        add_minute      => sub { $out = $dt->add( minutes => $offset ) },
-        subtract_minute => sub { $out = $dt->subtract( minutes => $offset ) },
-        add_second      => sub { $out = $dt->add( seconds => $offset ) },
-        subtract_second => sub { $out = $dt->subtract( seconds => $offset ) },
+        localtime       => sub { $out = scalar localtime $first },
+        dow             => sub { $out = $out_dt->day_name },
+        add_year        => sub { $out = $out_dt->add( years => $offset ) },
+        subtract_year   => sub { $out = $out_dt->subtract( years => $offset ) },
+        add_month       => sub { $out = $out_dt->add( months => $offset ) },
+        subtract_month  => sub { $out = $out_dt->subtract( months => $offset ) },
+        add_day         => sub { $out = $out_dt->add( days => $offset ) },
+        subtract_day    => sub { $out = $out_dt->subtract( days => $offset ) },
+        add_hour        => sub { $out = $out_dt->add( hours => $offset ) },
+        subtract_hour   => sub { $out = $out_dt->subtract( hours => $offset ) },
+        add_minute      => sub { $out = $out_dt->add( minutes => $offset ) },
+        subtract_minute => sub { $out = $out_dt->subtract( minutes => $offset ) },
+        add_second      => sub { $out = $out_dt->add( seconds => $offset ) },
+        subtract_second => sub { $out = $out_dt->subtract( seconds => $offset ) },
         difference      => sub {
-            if ( $first && $out ) {
-                my $date = UnixDate( $first, "%Y-%m-%eT%H:%M:%S" );
-                my $first_dt = DateTime::Format::DateParse->parse_datetime($date);
-                if ( $first_dt ) {
-                    $out = sprintf '%dy %dm %dd or %dh %dm %ds',
-                        $first_dt->delta_md($dt)->years,
-                        $first_dt->delta_md($dt)->months,
-                        $first_dt->delta_md($dt)->days,
-                        $first_dt->delta_ms($dt)->hours,
-                        $first_dt->delta_ms($dt)->minutes,
-                        $first_dt->delta_ms($dt)->seconds;
-                }
+            if ( $first_dt && $out_dt ) {
+                $out = sprintf '%dy %dm %dd or %dh %dm %ds',
+                    $first_dt->delta_md($out_dt)->years,
+                    $first_dt->delta_md($out_dt)->months,
+                    $first_dt->delta_md($out_dt)->days,
+                    $first_dt->delta_ms($out_dt)->hours,
+                    $first_dt->delta_ms($out_dt)->minutes,
+                    $first_dt->delta_ms($out_dt)->seconds;
                 $first = '';
             }
         },
-        localtime       => sub { $out = scalar localtime $first },
     };
 
     $dispatch->{$op}->();
